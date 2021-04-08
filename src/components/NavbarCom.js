b6d0627a5a80b15cc28d309403be0edd17b5be7e
shantovai23 <hasibulhasan23sept@gmail.com>
2021-04-08 21:50:51 +0600

    cart-style-stripe-toasify-added

--- src/components/NavbarCom.js
+++ src/components/NavbarCom.js
@@ -29,10 +29,10 @@ const NavbarCom = () => {
           </Nav.Link>
         </Nav>
         
-        <Form inline>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
-        </Form>
+        </Form> */}
       </Navbar.Collapse>
     </Navbar>
   );
--- src/components/OffersProducts.js
+++ src/components/OffersProducts.js
@@ -3,14 +3,16 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ProductsContext } from "../Global/ProductsContext";
-import Slider2 from '../components/Slider'
-import SpecialOffer from '../components/SpecialOffer'
-import {CartContext} from '../Global/CartContext'
+import Slider2 from "../components/Slider";
+import SpecialOffer from "../components/SpecialOffer";
+import { CartContext } from "../Global/CartContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide, Zoom, Flip, Bounce } from 'react-toastify';
+toast.configure();
 const Responsive = () => {
   const { offersProducts } = useContext(ProductsContext);
-  const {dispatch}=useContext(CartContext)
-  
-
+  const { dispatch } = useContext(CartContext);
 
   var settings = {
     dots: true,
@@ -21,8 +23,8 @@ const Responsive = () => {
     initialSlide: 0,
     adaptiveHeight: true,
     autoplay: true,
-    className:'slides',
-    autoplaySpeed:1200,
+    className: "slides",
+    autoplaySpeed: 1200,
     responsive: [
       {
         breakpoint: 1024,
@@ -52,29 +54,56 @@ const Responsive = () => {
   };
   return (
     <div>
-      <Slider2/>
+      <Slider2 />
       <h2 id="offersHeading" className="py-4">
         Grocery 5% off
       </h2>
-      <div id='customContainer'>
-      <Slider {...settings}>
-        {offersProducts.map((product) => (
-          <div id="customStyles" key={product.id}>
-            <div  className="card" style={{ width: "18rem"}}>
-              <img className="card-img-top" src={product.img} alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title" style={{fontWeight:'600'}}>{product.name}</h5>
-                <h5 className="card-title" style={{fontWeight:'600'}}>${product.price}.00</h5>
-                <a  className="btn btn-primary" style={{background:'#DA2267',textTransform:'capitalize'}} onClick={()=>dispatch({type:'ADD_TO_CART',id:product.id,product:product})}>
-                  Add to Cart
-                </a>
+      <div id="customContainer">
+        <Slider {...settings}>
+          {offersProducts.map((product) => (
+            <div id="customStyles" key={product.id}>
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  className="card-img-top"
+                  src={product.img}
+                  alt="Card image cap"
+                />
+                <div className="card-body">
+                  <h5 className="card-title" style={{ fontWeight: "600" }}>
+                    {product.name}
+                  </h5>
+                  <h5 className="card-title" style={{ fontWeight: "600" }}>
+                    ${product.price}.00
+                  </h5>
+                  <a
+                    className="btn btn-primary"
+                    style={{
+                      background: "#DA2267",
+                      textTransform: "capitalize",
+                    }}
+                    onClick={() => {
+                      dispatch({
+                        type: "ADD_TO_CART",
+                        id: product.id,
+                        product: product,
+                      });
+                     
+                        toast.success("Product Added", {
+                        position: toast.POSITION.TOP_RIGHT,
+                        transition:Zoom,    
+                      });
+                     
+                    }}
+                  >
+                    Add to Cart
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Slider>
+          ))}
+        </Slider>
       </div>
-      <SpecialOffer/>
+      <SpecialOffer />
     </div>
   );
 };
